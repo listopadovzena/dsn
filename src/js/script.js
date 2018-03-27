70c15a5304c22e056ab7b74b6dbd955b208e523a
imac <imac@MacBook-Air-imac.local>
2018-03-27 08:13:03 +0300

    d

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+
+
+
+$('#slider').slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 4,
+    autoplay:true,
+    arrows: false,
+
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+
+                centerMode: true,
+                centerPadding: '0px',
+
+                autoplay:true,
+                arrows: false,
+
+                slidesToShow: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '10px',
+                slidesToShow: 2,
+                autoplay:true
+
+            }
+        }
+    ]
+});
+
+
+
